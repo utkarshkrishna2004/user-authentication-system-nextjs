e2538cb0d1d34cba156f90b6bc65a78c5a197f15
utkarshkrishna2004 <utkarshmark14@gmail.com>
2023-10-03 10:49:44 +0530

    New commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return (
+        <div className="flex ">
+            <h1 className="text-center text-white  text-4xl">Login</h1>
+        </div>
+    )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUpPage() {
+    return (
+       <div className="flex ">
+          <h1 className="text-center text-white  text-4xl">Sign Up</h1>
+       </div>
+    );
+}

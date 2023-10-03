a156658c816595c678515e679b5f901e8acf114a
utkarshkrishna2004 <utkarshmark14@gmail.com>
2023-10-03 21:15:19 +0530

    New commit

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,21 +1,53 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { axios } from "axios";
+import  axios  from "axios";
+import toast from "react-hot-toast";
 
 export default function SignUpPage() {
+   const router = useRouter();
+
    const [user, setUser] = React.useState({
       email: "",
       password: "",
       username: "",
    });
+   const [buttonDisabled, setButtonDisabled] = React.useState(false);
 
-   const onSignup = async () => {};
+   const [loading, setLoading] = React.useState(false);
+
+   const onSignup = async () => {
+      try {
+         setLoading(true);
+         const response = await axios.post("/api/users/signup", user);
+         console.log("Sign Up success", response.data);
+         router.push("/login")
+         
+      } catch (error: any) {
+         console.log("Sign Up failed", error.message);
+         
+         toast.error(error.message);
+      } finally {
+         setLoading(false);
+      }
+   };
+
+   useEffect(() => {
+      if (
+         user.email.length > 0 &&
+         user.password.length > 0 &&
+         user.username.length > 0
+      ) {
+         setButtonDisabled(false);
+      } else {
+         setButtonDisabled(true);
+      }
+   }, [user]);
 
    return (
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
-         <h1 className=" text-4xl font-bold m-6">Sign Up</h1>
+         <h1 className=" text-4xl font-bold m-6">{loading ? "Processing..." : "Sign Up"}</h1>
          <hr />
          <label className=" text-lg font-medium" htmlFor="username">
             Enter Username
@@ -57,7 +89,7 @@ export default function SignUpPage() {
             onClick={onSignup}
             className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 mt-4 cursor-pointer transition-transform hover:bg-orange-500 hover:scale-110 hover:text-black hover:font-semibold hover:border-none  "
          >
-            Sign-Up Here
+            {buttonDisabled ? "No Sign Up" : "Sign Up"}
          </button>
          <Link
             href="/login"
